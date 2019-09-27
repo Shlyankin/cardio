62da881a5a8825fa7313029edd53f5ded7a1c803
shlyankin <shlyankin123@gmail.com>
2019-09-27 16:47:42 +0400

    +Hilbert Transform

--- PanTompkinsAlgorithm.py
+++ PanTompkinsAlgorithm.py
@@ -24,29 +24,11 @@ def PanTompkinsPipeline(batch_size=20, annot = "pan_tomp_annotation"):
             .run(batch_size=batch_size, shuffle=False, drop_last=False, n_epochs=1, lazy=True)
             )
 
-
-def low_freq_filter(signal):
-    result = np.zeros(len(signal) - 12)
-    for index in range(len(result)):
-        result[index] = signal[index + 12] - 2 * signal[index + 6] + signal[index]
-        if index > 1:
-            result[index] += 2 * result[index - 1]
-        if index > 2:
-            result[index] -= result[index - 2]
-    return result
-
-
-def high_freq_filter(signal):
-    result = np.zeros(len(signal) - 32)
-    for index in range(len(result)):
-        result[index] = 32*signal[index + 16] + 32 * signal[index + 32] - signal[index]
-        if index > 1:
-            result[index] -= result[index - 1]
-    return result
-
-
-def compute_derivative(signal):
-    result = np.zeros(len(signal) - 4)
-    for index in range(len(result)):
-        result[index] = 0.125 * (2*signal[index + 4] + signal[index + 3] - signal[index + 1] - 2*signal[signal])
-    return result
+def HilbertTransformPipeline(batch_size=20, annot = "hilbert_annotation"):
+    return (bf.Pipeline()
+            .init_variable(annot, init_on_each_run=list)
+            .load(fmt='wfdb', components=["signal", "meta"])
+            .band_pass_signals(8, 20)
+            .hilbert_transform(dst=annot)
+            .update_variable(annot, bf.B(annot), mode='e')
+            .run(batch_size=batch_size, shuffle=False, drop_last=False, n_epochs=1, lazy=True))
